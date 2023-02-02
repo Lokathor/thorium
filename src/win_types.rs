050534a81834640436b59216a799abc4bdddbdc2
Lokathor <zefria@gmail.com>
2023-02-01 22:28:56 -0700

    printing for zwstring

--- src/win_types.rs
+++ src/win_types.rs
@@ -69,7 +69,7 @@ impl HANDLE {
   }
 }
 
-#[derive(Debug, Clone)]
+#[derive(Clone)]
 #[repr(transparent)]
 pub struct ZWString(Vec<u16>);
 impl ZWString {
@@ -82,6 +82,29 @@ impl ZWString {
   #[must_use]
   pub fn as_mut_ptr(&mut self) -> *mut u16 {
     self.0.as_mut_ptr()
+  }
+  #[inline]
+  #[must_use]
+  pub fn chars<'a>(&'a self) -> impl Iterator<Item = char> + 'a {
+    let live_slice: &[u16] = &self.0[..(self.0.len() - 1)];
+    core::char::decode_utf16(live_slice.iter().copied())
+      .map(|r| r.unwrap_or(char::REPLACEMENT_CHARACTER))
+  }
+}
+impl core::fmt::Debug for ZWString {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    write!(f, "\"")?;
+    core::fmt::Display::fmt(self, f)?;
+    write!(f, "\"")?;
+    Ok(())
+  }
+}
+impl core::fmt::Display for ZWString {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    for ch in self.chars() {
+      write!(f, "{ch}")?;
+    }
+    Ok(())
   }
 }
 impl From<&str> for ZWString {
